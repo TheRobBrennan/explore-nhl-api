0ab4cc3894ce6a6bba8dbeb14156ac1315b0de04
Rob Brennan <rob@therobbrennan.com>
2023-01-22 12:46:55 -0800

    Add development comments for notes and planned enhancements

--- examples/NHLShotChart.py
+++ examples/NHLShotChart.py
@@ -24,10 +24,17 @@ NHL_API_DATE_TIME_FORMAT_STRING = "%Y-%m-%dT%H:%M:%SZ"  # '2023-01-20T03:00:00Z'
 
 # NHL settings and configuration
 NHL_SEASON = 20222023
-NHL_TEAM_ID = 55  # Seattle Kraken
+NHL_TEAM_ID_SEATTLE_KRAKEN = 55
 
-# NHL_GAME_ID = 2022020728  # 2023.01.19 => NJD @ SEA
-NHL_GAME_ID = 2022020743  # 2023.01.21 => COL @ SEA
+# Known issue where home shots on goal do not match expected 33 shots (from NHL link below)
+NHL_GAME_ID = 2022020711  # 2023.01.17 => SEA @ EDM - https://www.nhl.com/gamecenter/sea-vs-edm/2023/01/17/2022020711#game=2022020711,game_state=final,lock_state=final,game_tab=stats
+# Expected 31 SOG for SEA / 33 SOG for EDM; actual data from the NHL API shows 31 SOG for SEA / 34 SOG for EDM
+
+# NHL_GAME_ID = 2022020728  # 2023.01.19 => NJD @ SEA - https://www.nhl.com/gamecenter/njd-vs-sea/2023/01/19/2022020728/recap/stats#game=2022020728,game_state=final,lock_state=final,game_tab=stats
+# Expected 30 SOG for NJD / 40 SOG for SEA
+
+# NHL_GAME_ID = 2022020743  # 2023.01.21 => COL @ SEA - https://www.nhl.com/gamecenter/col-vs-sea/2023/01/21/2022020743/recap/stats#game=2022020743,game_state=final,lock_state=final,game_tab=stats
+# Expected 27 SOG for COL / 27 SOG for SEA
 
 # Date and time
 START_DATE = "2023-01-19"
@@ -36,58 +43,107 @@ LOCAL_DATE_TIME_FORMAT_STRING = "YYYY-MM-DD h:mma"  # '2023-01-19 7:00pm'
 OUTPUT_SEPARATOR = "\n\n*****\n\n"
 
 
+# Utility method to either log or pretty print JSON data
 def printJSON(data, indent=0):
     if indent == 0:
         print(OUTPUT_SEPARATOR + data + OUTPUT_SEPARATOR)
     else:
         print(OUTPUT_SEPARATOR + json.dumps(data,
-              indent=indent) + OUTPUT_SEPARATOR)
+                                            indent=indent) + OUTPUT_SEPARATOR)
 
 
+# Generate a shot chart for a specific game ID from the NHL API
 def generate_shot_chart_for_game(gameId):
+    # TODO - Write a function that parses content and returns values for generating a shot chart
+
+    # --------------------------------------------------------------------------------------------------------
+    # Load data for a specific game ID from the NHL API
+    # --------------------------------------------------------------------------------------------------------
     content = load_live_data_for_game(gameId)
 
+    # Contains details about the game, status, teams, players, and venue
     game_data = content["gameData"]
-    event = content["liveData"]
-    date = game_data["datetime"]
 
-    linescore = event["linescore"]
+    # Contains plays, linescore, box score, and decisions from the game
+    eventDescription = content["liveData"]
+    # --------------------------------------------------------------------------------------------------------
+
+    # --------------------------------------------------------------------------------------------------------
+    # Contains current period information, shootout details if applicable, and more
+    linescore = eventDescription["linescore"]
+
+    # Use the linescore object to determine
+    # - If the game is complete
     currentPeriodTimeRemaining = linescore["currentPeriodTimeRemaining"]
+    # - The current period, or Final / OT where applicable
     currentPeriodOrdinal = linescore["currentPeriodOrdinal"]
+    # --------------------------------------------------------------------------------------------------------
 
+    # --------------------------------------------------------------------------------------------------------
+    # Contains the start and end time for the game
+    date = game_data["datetime"]
+    # --------------------------------------------------------------------------------------------------------
     # Convert '2023-01-20T03:00:00Z' to '2023-01-19 7:00pm'
-    datetime = arrow.get(dt.strptime(
+    # TODO - Write a function to convert the NHL API date time format to our local date time format
+    gameStartLocalDateTime = arrow.get(dt.strptime(
         date["dateTime"], NHL_API_DATE_TIME_FORMAT_STRING)).to('local').format(LOCAL_DATE_TIME_FORMAT_STRING)
+    # --------------------------------------------------------------------------------------------------------
+
+    # --------------------------------------------------------------------------------------------------------
+    # Team details
+    # --------------------------------------------------------------------------------------------------------
     teams = game_data["teams"]
     away = teams["away"]
     home = teams["home"]
     away_team = away["abbreviation"]
     home_team = home["abbreviation"]
     print(teams)
-    plays = event["plays"]
+    # --------------------------------------------------------------------------------------------------------
+
+    # --------------------------------------------------------------------------------------------------------
+    # Plays
+    # --------------------------------------------------------------------------------------------------------
+    plays = eventDescription["plays"]
     all_plays = plays["allPlays"]
+    # --------------------------------------------------------------------------------------------------------
 
-    fig = plt.figure(figsize=(10, 10))
-    plt.xlim([0, 100])
-    plt.ylim([-42.5, 42.5])
-
+    # --------------------------------------------------------------------------------------------------------
+    # Initialize counters for processing all plays from the game
+    # --------------------------------------------------------------------------------------------------------
     home_shot_attempts = 0
     home_sog = 0
     home_goals = 0
     away_shot_attempts = 0
     away_sog = 0
     away_goals = 0
-    for i in all_plays:
-        result = i["result"]
-        event = result["event"]
-        if event == "Goal" or event == "Shot" or event == "Missed Shot":
-            team_info = i["team"]
+
+    # Define our scatter plot
+    fig = plt.figure(figsize=(10, 10))
+    plt.xlim([0, 100])
+    plt.ylim([-42.5, 42.5])
+    # --------------------------------------------------------------------------------------------------------
+
+    # --------------------------------------------------------------------------------------------------------
+    # Process all event data
+    # --------------------------------------------------------------------------------------------------------
+    for event in all_plays:
+
+        # --------------------------------------------------------------------------------------------------------
+        # TODO - Parse event details
+        # --------------------------------------------------------------------------------------------------------
+        result = event["result"]  # Details for the event
+
+        # Description of the event (e.g. Game Scheduled, Goal, Shot, Missed Shot, etc.)
+        eventDescription = result["event"]
+
+        if eventDescription == "Goal" or eventDescription == "Shot" or eventDescription == "Missed Shot":
+            team_info = event["team"]
             team = team_info["triCode"]
             if team == home_team:
                 home_shot_attempts += 1
                 print(team)
-                print(event)
-                coords = (i["coordinates"])
+                print(eventDescription)
+                coords = (event["coordinates"])
                 print(coords)
                 x = int(coords["x"])
                 y = int(coords["y"])
@@ -97,11 +153,11 @@ def generate_shot_chart_for_game(gameId):
                 else:
                     x = x
                     y = y
-                print(i)
-                if event == "Goal":
+                print(event)
+                if eventDescription == "Goal":
                     try:
-                        print(i["about"]["periodType"])
-                        if i["about"]["periodType"] != 'SHOOTOUT':
+                        print(event["about"]["periodType"])
+                        if event["about"]["periodType"] != 'SHOOTOUT':
                             home_sog += 1
 
                         home_goals += 1
@@ -112,7 +168,7 @@ def generate_shot_chart_for_game(gameId):
                             plt.plot(x, y, 'd', color="#4bad53", markersize=20)
                     except:
                         plt.plot(x, y, 'd', color="#4bad53", markersize=20)
-                elif event == "Shot":
+                elif eventDescription == "Shot":
                     home_sog += 1
                     plt.plot(x, y, 'o', color="#f0a911", markersize=15)
                 else:
@@ -120,8 +176,8 @@ def generate_shot_chart_for_game(gameId):
             else:
                 away_shot_attempts += 1
                 print(team)
-                print(event)
-                coords = (i["coordinates"])
+                print(eventDescription)
+                coords = (event["coordinates"])
                 print(coords)
                 x = int(coords["x"])
                 y = int(coords["y"])
@@ -131,11 +187,11 @@ def generate_shot_chart_for_game(gameId):
                 else:
                     x = x
                     y = y
-                print(i)
-                if event == "Goal":
+                print(event)
+                if eventDescription == "Goal":
                     try:
-                        print(i["about"]["periodType"])
-                        if i["about"]["periodType"] != 'SHOOTOUT':
+                        print(event["about"]["periodType"])
+                        if event["about"]["periodType"] != 'SHOOTOUT':
                             away_sog += 1
 
                         away_goals += 1
@@ -146,37 +202,51 @@ def generate_shot_chart_for_game(gameId):
                             plt.plot(x, y, 'd', color="#4bad53", markersize=20)
                     except:
                         plt.plot(x, y, 'd', color="#4bad53", markersize=20)
-                elif event == "Shot":
+                elif eventDescription == "Shot":
                     away_sog += 1
                     plt.plot(x, y, 'o', color="#f0a911", markersize=15)
                 else:
                     plt.plot(x, y, 'x', color="#000000", markersize=15)
         else:
             continue
+        # --------------------------------------------------------------------------------------------------------
 
+    # --------------------------------------------------------------------------------------------------------
+
+    # Use a predefined NHL rink with markings to overlay our scatter plot
     rink = NHLRink()
     ax = rink.draw()
 
+    # --------------------------------------------------------------------------------------------------------
+    # TODO: Generate a title for our shot chart
+    # --------------------------------------------------------------------------------------------------------
     # Build our title
     title_line1 = away_team + " (" + str(away_goals) + ")" + \
         " vs. " + home_team + " (" + str(home_goals) + ")" + "\n"
-    title_line2 = datetime + "\n"
+    title_line2 = gameStartLocalDateTime + "\n"
     title_line3 = currentPeriodTimeRemaining + "/" + currentPeriodOrdinal + "\n\n"
     title_line4 = away_team + ": " + str(away_sog) + " SOG (" + str(away_shot_attempts) + " Total Shot Attempts)\n" + \
         home_team + ": " + str(home_sog) + " SOG (" + \
         str(home_shot_attempts) + " Total Shot Attempts)"
     title = title_line1 + title_line2 + title_line3 + title_line4
+    # --------------------------------------------------------------------------------------------------------
 
-    # Add title and display chart
+    # --------------------------------------------------------------------------------------------------------
+    # TODO: Add the title, save our chart as a PNG, and display it to the user
+    # --------------------------------------------------------------------------------------------------------
+    # Add title
     plt.title(title)
 
     # OPTIONAL: Save our plot to a PNG file
-    plt.savefig('./images/shot-chart-' + str(gameId) + '-' + datetime.replace(' ', '_') + '-' +
+    plt.savefig('./images/shot-chart-' + str(gameId) + '-' + gameStartLocalDateTime.replace(' ', '_') + '-' +
                 away_team + '-vs-' + home_team + '.png')
 
+    # Display chart
     plt.show()
+    # --------------------------------------------------------------------------------------------------------
 
 
+# Load live data for a specific game ID from the NHL API
 def load_live_data_for_game(gameId):
     # https://statsapi.web.nhl.com/api/v1/game/2022020728/feed/live
     NHL_API_LIVE_GAME_DATA_URL = NHL_API_BASE_URL + \
@@ -186,6 +256,7 @@ def load_live_data_for_game(gameId):
     return live_data
 
 
+# Load schedule data for a specific season and team ID from the NHL API
 def load_schedule_for_season_and_team(season, teamId):
     # Build our schedule URL using team information from above
     NHL_API_SCHEDULE_URL = NHL_API_BASE_URL + \
@@ -195,6 +266,7 @@ def load_schedule_for_season_and_team(season, teamId):
     printJSON(kraken_schedule, 1)
 
 
+# Load schedule data using a specified start and end date for a specific team ID with an optional hydrate CSV string from the NHL API
 def load_schedule_for_team_with_start_and_end_dates(teamId, startDate, endDate, hydrateCSVString=""):
     # https://statsapi.web.nhl.com/api/v1/schedule?startDate=2023-01-19&endDate=2023-01-21&hydrate=team,linescore,metadata,seriesSummary(series)&teamId=55
     # Build our partial schedule URL using team information from above
